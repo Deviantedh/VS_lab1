926659105d47dbf52fcb389a8b11f7d47df2826b
AlexandroLe <alex-yastrebov2003-2015@mail.ru>
2026-09-24 23:24:18 +0400

    feat: добавил интеграционный тест + небольшие поправки

--- src/test/java/portal/Lab1ApplicationTests.java
+++ src/test/java/portal/Lab1ApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class Lab1ApplicationTests {
+public class Lab1ApplicationTests {
 
     @Test
     void contextLoads() {
